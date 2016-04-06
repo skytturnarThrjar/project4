3ed5821ccb1c06f191d301d4226f6c8209d10ebf
vala14 <vala14@ru.is>
2016-04-06 12:03:47 +0000

    laga smá

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -45,13 +45,12 @@ window.Game = (function() {
 	 * Starts a new game.
 	 */
 	Game.prototype.start = function() {
-		this.reset();
-
 		// Restart the onFrame loop
 		this.lastFrame = +new Date() / 1000;
 		window.requestAnimationFrame(this.onFrame);
 		this.isPlaying = true;
 		document.getElementById('backgroundSong').volume = 0.5;
+		this.reset();
 	};
 
 	/**
@@ -67,9 +66,9 @@ window.Game = (function() {
 	 */
 	Game.prototype.gameover = function() {
 		this.isPlaying = false;
-		this.reset();
 		// Should be refactored into a Scoreboard class.
 		var that = this;
+		that.pipe.reset();
 		var scoreboardEl = this.el.find('.Scoreboard');
 		scoreboardEl
 			.addClass('is-visible')
--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -16,32 +16,34 @@ if (!window.requestAnimationFrame) {
 }
 
 var mute = function() {
-  'use strict';
-  var audioClass = document.getElementById('backgroundSong').className;
-  if(audioClass === 'on') {
-    document.getElementById('backgroundSong').pause();
-    document.getElementById('backgroundSong').className = 'off';
-    document.getElementById('yoSound').className = 'off';
-    document.getElementById('laugh').className = 'off';
-  }
-  else if(audioClass === 'off') {
-    document.getElementById('backgroundSong').play();
-    document.getElementById('backgroundSong').volume = 0.5;
-    document.getElementById('backgroundSong').className = 'on';
-    document.getElementById('yoSound').className = 'on';
-    document.getElementById('laugh').className = 'on';
-  }
+
+    'use strict';
+    var audioClass = document.getElementById('backgroundSong').className;
+    if(audioClass === 'on') {
+        document.getElementById('backgroundSong').pause();
+        document.getElementById('backgroundSong').className = 'off';
+        document.getElementById('yoSound').className = 'off';
+        document.getElementById('laugh').className = 'off';
+    }
+    else if(audioClass === 'off') {
+        document.getElementById('backgroundSong').play();
+        document.getElementById('backgroundSong').volume = 0.5;
+        document.getElementById('backgroundSong').className = 'on';
+        document.getElementById('yoSound').className = 'on';
+        document.getElementById('laugh').className = 'on';
+    }
 };
 
 var changeImage = function() {
-  'use strict';
-  var imageClass = document.getElementById('muteImage').className;
-  if(imageClass === 'on') {
-    document.getElementById('muteImage').className = 'off';
-    document.getElementById('muteImage').src = '/images/volumeMute.png';
-  }
-  else if(imageClass === 'off') {
-    document.getElementById('muteImage').className = 'on';
-    document.getElementById('muteImage').src = '/images/volume.png';
-  }
-}
+
+    'use strict';
+    var imageClass = document.getElementById('muteImage').className;
+    if(imageClass === 'on') {
+        document.getElementById('muteImage').className = 'off';
+        document.getElementById('muteImage').src = '/images/volumeMute.png';
+    }
+    else if(imageClass === 'off') {
+        document.getElementById('muteImage').className = 'on';
+        document.getElementById('muteImage').src = '/images/volume.png';
+    }
+};
